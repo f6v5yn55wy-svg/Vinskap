c3e300a18a29df1d26e49d039ecd388f9d7be52e
f6v5yn55wy-svg <f6v5yn55wy@privaterelay.appleid.com>
2026-09-17 13:25:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vinskap-v11';
-const ASSETS=['/','/index.html','/styles.css','/styles-v11.css','/app-v11.js','/manifest.json','/icon.svg'];
+const CACHE='vinskap-v12';
+const ASSETS=['/','/index.html','/styles.css','/styles-v12.css','/app-v12.js','/manifest.json','/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
